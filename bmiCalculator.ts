3ae647f9395019dc577f3feab72496d3dd8f466a
ShahSau <shahriarksaurov@gmail.com>
2022-10-14 22:06:10 +0300

    9.5 WebBMI

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,41 +1,89 @@
-
-const calculateBmi = (height:number, weight:number):string=>{
+interface Bmi {
+    weight: Number,
+    height: Number,
+    bmi: String
+}
+export const calculateBmi = (height:number, weight:number):Bmi=>{
     let output:number = weight / (height /100)**2
-    if(output <15){
-        return 'Very severely underweight'
-    }else if(output > 15 && output <16){
-        return 'Severely underweight'
-    }else if(output > 16 && output <18.5){
-        return 'Underweight'
-    }else if(output > 18.5 && output <25){
-        return 'Normal (healthy weight)'
-    }else if(output > 25 && output <30){
-        return 'Overweight'
-    }else if(output > 30 && output <35){
-        return 'Moderately obese'
-    }else if(output > 35 && output <40){
-        return 'Severely obese'
-    }else if(output>40){
-        return 'Very severely obese'
-    }
-} 
-try {
-    const a: number = Number(process.argv[2])
-    const b: number = Number(process.argv[3])
-    if(process.argv.length === 4){
-    if(!(isNaN(a))&& !(isNaN(b))){
-        console.log(calculateBmi(a,b))
-    }else{
-        console.log('Failed to calculate BMI: inputs must be numbers')
-    }
-    }else{
-        console.log('Failed to calculate BMI: two inputs are needed')
+    try {
+        if(output <15){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Very severely underweight'
+            }
+        }else if(output > 15 && output <16){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Severely underweight'
+            }
+        }else if(output > 16 && output <18.5){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Underweight'
+            }
+        }else if(output > 18.5 && output <25){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Normal (healthy weight)'
+            }
+        }else if(output > 25 && output <30){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Overweight'
+            }
+        }else if(output > 30 && output <35){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Moderately obese'
+            }
+        }else if(output > 35 && output <40){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Severely obese'
+            }
+        }else if(output>40){
+            return {
+                weight: weight,
+                height: height,
+                bmi: 'Very severely obese'
+            }
+        }else{
+            return{
+                weight: weight,
+                height: height,
+                bmi: 'something went wrong in calculation'
+            }
+        }
+        
+    } catch (error) {
+        throw(error)
     }
     
-} catch (error:unknown) {
-    let errorMessage = 'Failed to calculate BMI,'
-    if(error instanceof Error){
-        errorMessage += ' because: ' + error.message
-    }
-    console.log(errorMessage);
-}
+} 
+// try {
+//     const a: number = Number(process.argv[2])
+//     const b: number = Number(process.argv[3])
+//     if(process.argv.length === 4){
+//     if(!(isNaN(a))&& !(isNaN(b))){
+//         console.log(calculateBmi(a,b))
+//     }else{
+//         console.log('Failed to calculate BMI: inputs must be numbers')
+//     }
+//     }else{
+//         console.log('Failed to calculate BMI: two inputs are needed')
+//     }
+    
+// } catch (error:unknown) {
+//     let errorMessage = 'Failed to calculate BMI,'
+//     if(error instanceof Error){
+//         errorMessage += ' because: ' + error.message
+//     }
+//     console.log(errorMessage);
+// }
--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send( 'Hello Full Stack!');
 });
 
+app.get('/bmi', (req,res)=>{
+  let {height, weight} = req.query
+try {
+    const a: number = Number(height)
+    const b: number = Number(weight)
+    if(!(isNaN(a))&& !(isNaN(b))){
+        res.send(calculateBmi(a,b))
+    }else{
+      res.status(400)
+      res.send({
+        error: "malformatted parameters"
+      })
+    }   
+} catch (error:unknown) {
+  res.status(400)
+  res.send({
+    error: "malformatted parameters"
+  })
+
+}
+})
 const PORT = 3003;
 
 app.listen(PORT, () => {
