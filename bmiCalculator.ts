39768c6f16358b188f355e398af211b341bd244e
ShahSau <shahriarksaurov@gmail.com>
2022-10-14 23:08:21 +0300

    9.6 Eslint

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,89 +1,64 @@
 interface Bmi {
-    weight: Number,
-    height: Number,
-    bmi: String
+    weight: number,
+    height: number,
+    bmi: string
 }
 export const calculateBmi = (height:number, weight:number):Bmi=>{
-    let output:number = weight / (height /100)**2
-    try {
+    const output:number = weight / (height /100)**2;
         if(output <15){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Very severely underweight'
-            }
+            };
         }else if(output > 15 && output <16){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Severely underweight'
-            }
+            };
         }else if(output > 16 && output <18.5){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Underweight'
-            }
+            };
         }else if(output > 18.5 && output <25){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Normal (healthy weight)'
-            }
+            };
         }else if(output > 25 && output <30){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Overweight'
-            }
+            };
         }else if(output > 30 && output <35){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Moderately obese'
-            }
+            };
         }else if(output > 35 && output <40){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Severely obese'
-            }
+            };
         }else if(output>40){
             return {
                 weight: weight,
                 height: height,
                 bmi: 'Very severely obese'
-            }
+            };
         }else{
             return{
                 weight: weight,
                 height: height,
                 bmi: 'something went wrong in calculation'
-            }
+            };
         }
-        
-    } catch (error) {
-        throw(error)
-    }
     
-} 
-// try {
-//     const a: number = Number(process.argv[2])
-//     const b: number = Number(process.argv[3])
-//     if(process.argv.length === 4){
-//     if(!(isNaN(a))&& !(isNaN(b))){
-//         console.log(calculateBmi(a,b))
-//     }else{
-//         console.log('Failed to calculate BMI: inputs must be numbers')
-//     }
-//     }else{
-//         console.log('Failed to calculate BMI: two inputs are needed')
-//     }
-    
-// } catch (error:unknown) {
-//     let errorMessage = 'Failed to calculate BMI,'
-//     if(error instanceof Error){
-//         errorMessage += ' because: ' + error.message
-//     }
-//     console.log(errorMessage);
-// }
+};
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -7,7 +7,7 @@ interface Exercise {
     target: number,
     average: number
 }
-let description:Array<string> =[
+const description:Array<string> =[
     'focus on exercise',
     'better luck next time',
     'not too bad but could be better',
@@ -15,12 +15,11 @@ let description:Array<string> =[
     'Great job',
     'you did it!'
 
-]
+];
 const calculateExercise = (hours:Array<number>, goal:number):Exercise =>{
-    let exerciseDays:Array<number> = hours.filter(hour => hour !== 0)
-    let totalhours:number = hours.reduce((partial, sum)=> partial + sum, 0)
-    let ratingD:number = Math.round(totalhours / hours.length)>5 ? 5 : Math.round(totalhours / hours.length)
-    console.log('total hours', totalhours)
+    const exerciseDays:Array<number> = hours.filter(hour => hour !== 0);
+    const totalhours:number = hours.reduce((partial, sum)=> partial + sum, 0);
+    const ratingD:number = Math.round(totalhours / hours.length)>5 ? 5 : Math.round(totalhours / hours.length);
     return {
         periodLength : hours.length,
         trainingDays :exerciseDays.length,
@@ -29,26 +28,26 @@ const calculateExercise = (hours:Array<number>, goal:number):Exercise =>{
         ratingDescription:description[ratingD],
         target:goal,
         average:totalhours / hours.length
-    }
-}
+    };
+};
 
 try {
-    const target: number = Number(process.argv[2])
-    const work: Array<number> = []
+    const target = Number(process.argv[2]);
+    const work: Array<number> = [];
     
     if(process.argv.length >= 4){
         for (let i = 3; i < process.argv.length; i++) {
-            work.push(parseFloat(process.argv[i]))
+            work.push(parseFloat(process.argv[i]));
         }
-        console.log(calculateExercise(work, target))
+        console.log(calculateExercise(work, target));
     }else{
-        console.log('minimum 2 arguments are needed')
+        console.log('minimum 2 arguments are needed');
     }
     
 } catch (error:unknown) {
-    let errorMessage = 'Failed to calculate BMI,'
+    let errorMessage = 'Failed to calculate BMI,';
     if(error instanceof Error){
-        errorMessage += ' because: ' + error.message
+        errorMessage += ' because: ' + error.message;
     }
     console.log(errorMessage);
 }
