8e4d40db4540d55007378eef95914c44a7230ae1
shahriar <shahriarksaurov@gmail.com>
2022-10-30 12:36:25 +0200

    9.15.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,38 +1,134 @@
 const App = () => {
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+interface CourseDescriptionPart extends CoursePartBase{
+  description:string;
+}
+interface CourseSpecialPart extends CourseDescriptionPart{
+  type:"special";
+  requirements: Array<string>
 
-  const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+}
+interface CourseNormalPart extends CourseDescriptionPart {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+interface CourseSubmissionPart extends CourseDescriptionPart {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+//for rendering
+interface CourseAll extends CoursePartBase{
+  description?:string;
+  requirements?: Array<string>
+  groupProjectCount?: number;
+  exerciseSubmissionLink?: string;
+}
 
-  const TotaSum  = courseParts.reduce((carry, part) => carry + part.exerciseCount, 0);
-  const Header = ({header}:{header:string})=>{
-    return <h1>{header}</h1>;
-  };
-  const Content = ({name,exercise}:{name:string,exercise:number})=>{
-    return <p>{name} {exercise}</p>;
-  };
-  const Total = ({sum}:{sum:number})=>{
-    return <p>Number of exercises{" "}{sum}</p>;
-  };
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+// this is the new coursePart variable
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the easy course part",
+    type: "normal"
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the hard course part",
+    type: "normal"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: "groupProject"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: "submission"
+  },
+  {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "jest"],
+    type: "special"
+  }
+];
+const courseName = "Half Stack application development";
+const TotaSum  = courseParts.reduce((carry, part) => carry + part.exerciseCount, 0);
+
+//header
+const Header = ({header}:{header:string})=>{
+  return <h1>{header}</h1>;
+};
+
+
+type CourseAllN = Omit<CourseAll, "type">;
+const Content = ({name,exerciseCount,description,exerciseSubmissionLink,groupProjectCount,requirements}: CourseAllN )=>{
+  const str = requirements?.join(', ');
+  return(
+  <>
+    <h2>{name} {exerciseCount}</h2>
+    <p>{description}</p>
+    {groupProjectCount && <p>project exercises {groupProjectCount}</p>}
+    {exerciseSubmissionLink && <p>submit to {exerciseSubmissionLink}</p>}
+    {requirements && <p>required skills {str}</p>}
+  </>);
+};
+//total
+const Total = ({sum}:{sum:number})=>{
+  return <p>Number of exercises{" "}{sum}</p>;
+};
+/**
+ * Helper function for exhaustive type checking
+ */
+ const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
   return (
     <div>
+      <>
       <Header header={courseName} />
-      <Content name={courseParts[0].name} exercise={courseParts[0].exerciseCount} />
-      <Content name={courseParts[1].name} exercise={courseParts[1].exerciseCount} />
-      <Content name={courseParts[2].name} exercise={courseParts[2].exerciseCount} />
+      {courseParts.map((part,index)=>{
+        switch(part.type){
+          case "normal":
+            return <Content key={index} name={part.name} exerciseCount={part.exerciseCount} description={part.description} />;
+            break;
+          case "groupProject":
+            return <Content key={index} name={part.name} exerciseCount={part.exerciseCount} groupProjectCount={part.groupProjectCount}/>;
+            break;
+          case "submission":
+            return <Content key={index} name={part.name} exerciseCount={part.exerciseCount} description={part.description} exerciseSubmissionLink ={part.exerciseSubmissionLink}/>;
+            break;
+          case "special":
+            return <Content key={index} name={part.name} exerciseCount={part.exerciseCount} description={part.description} requirements={part.requirements}/>;
+            break;
+          default:
+            return assertNever(part);
+        }
+      }
+      )}
+      
       <Total sum={TotaSum} />
+      </>
     </div>
   );
 };
